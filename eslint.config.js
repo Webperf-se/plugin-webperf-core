6a83badaca0668133c0ed5bfbbc2c7e18a60a52b
7h3Rabbit <62792609+7h3Rabbit@users.noreply.github.com>
2024-06-01 16:31:24 +0200

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,43 +1,26 @@
-// eslint.config.js
+import js from '@eslint/js';
 
-import prettier from 'eslint-plugin-prettier';
-import unicorn from 'eslint-plugin-unicorn';
-
-export default {
-  languageOptions: {
-    globals: {
-      node: true,
-      es6: true
+export default [
+  {
+    languageOptions: {
+      globals: {
+        node: true,
+        es6: true
+      },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
     },
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module'
+    rules: {
+      'require-atomic-updates': 0,
+      'no-extra-semi': 0,
+      'no-mixed-spaces-and-tabs': 0,
+      'unicorn/filename-case': 0,
+      'unicorn/prevent-abbreviations': 0,
+      'unicorn/no-array-reduce': 0,
+      'unicorn/prefer-spread': 0
     }
   },
-  plugins: {
-    prettier,
-    unicorn
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:unicorn/recommended'
-  ],
-  rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true,
-        trailingComma: 'none',
-        arrowParens: 'avoid',
-        embeddedLanguageFormatting: 'off'
-      }
-    ],
-    'require-atomic-updates': 0,
-    'no-extra-semi': 0,
-    'no-mixed-spaces-and-tabs': 0,
-    'unicorn/filename-case': 0,
-    'unicorn/prevent-abbreviations': 0,
-    'unicorn/no-array-reduce': 0,
-    'unicorn/prefer-spread': 0
-  }
-}
+  js.configs.recommended
+];
